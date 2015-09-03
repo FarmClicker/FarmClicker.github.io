912ddc5a9ca21bd21cf12e685fb929154e8ff2c3
Mahtab <mahtabz@gmail.com>
2015-09-03 17:06:41 -0400

    Changed images

--- modules/crops/CropsData.js
+++ modules/crops/CropsData.js
@@ -1,5 +1,5 @@
 [
-  {"name":"Wheat", "image":"../../assets/img/wheat.png", "bought":false, "price":200, "perClick":10},
+  {"name":"Wheat", "image":"../../assets/img/wheat.png", "bought":false, "price":100, "perClick":10},
   {"name":"Corn", "image":"../../assets/img/corn.png", "bought":false, "price":500, "perClick":100},
-  {"name":"Carrots", "image":"../../assets/img/carrot.png", "bought":false, "price":1000, "perClick":600}
+  {"name":"Carrots", "image":"../../assets/img/carrot.png", "bought":false, "price":5000, "perClick":500}
 ]
