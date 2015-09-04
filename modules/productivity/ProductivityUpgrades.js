8ed4f9192d0233329032f036155a385c3f44e674
Mahtab <mahtabz@gmail.com>
2015-09-04 14:08:29 -0400

    Added new upgrade

--- modules/productivity/ProductivityUpgrades.js
+++ modules/productivity/ProductivityUpgrades.js
@@ -1,5 +1,6 @@
 [
-  {"name":"Hire Worker", "image":"../../assets/img/worker.png", "bought":0, "price":10, "persecond":1, "increase":0.5},
+  {"name":"Buy Pitchfork", "image":"../../assets/img/Pitchfork.png", "bought":0, "price":10, "persecond":0.1, "increase":0.5},
+  {"name":"Hire Worker", "image":"../../assets/img/worker.png", "bought":0, "price":30, "persecond":1, "increase":0.5},
   {"name":"Buy Work Horse", "image":"../../assets/img/horse-head.png", "bought":0, "price":100, "persecond":10, "increase":0.5},
   {"name":"Buy Tractor", "image":"../../assets/img/tractor.png", "bought":0, "price":1000, "persecond":100, "increase":0.5}
 ]
