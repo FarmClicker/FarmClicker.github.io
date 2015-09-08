132f378754c3689def4d44439e00d8db15f4ebe0
Mahtab <mahtabz@gmail.com>
2015-09-08 15:35:17 -0400

    Added a navbar and about page

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
-var FarmClickerApp = angular.module('FarmClickerApp', []);
+var FarmClickerApp = angular.module('FarmClickerApp', ['ngRoute']);
 //TODO: routing
+FarmClickerApp.config(function($routeProvider){
+  $routeProvider
+  .when('/', {
+    controller: 'ApplicationController',
+    templateUrl: 'modules/application/app.html'
+  })
+  .when('/about', {
+    templateUrl: 'modules/about/about.html'
+  })
+  .otherwise({
+    redirectTo: '/'
+  });
+});
